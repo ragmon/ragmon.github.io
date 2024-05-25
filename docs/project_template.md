2321320129166a43676584b5524c10722fd34ae9
Arthur Rahimov <arthur.ragimov@gmail.com>
2024-05-25 16:44:33 +0200

    update

---
+++ docs/project_template.md
@@ -0,0 +1,57 @@
+## Project Template Documentation
+
+This document outlines the structure and key components of a project entry in a portfolio.
+
+### Project Details:
+
+- **Title**: The title/name of the project.
+- **Description**: A brief description of the project.
+- **Start Date**: The start date of the project.
+- **Technology Stack**: Technologies and tools used in the project.
+- **Your Role**: Your role or contribution to the project.
+- **Project Type**: Type or category of the project (e.g., Website, Mobile App, API).
+- **Project URL**: Link to the project (if available).
+
+### Media Assets:
+
+- **Logo**: Logo or visual representation of the project (includes image source and alternative text).
+- **Photos**: Images or screenshots showcasing the project (includes image URLs and alternative text).
+
+### Example JSON Structure:
+
+```json
+{
+  "title": "Project Title",
+  "description": "Brief project description.",
+  "start_date": "YYYY-MM-DD",
+  "tech_stack": ["Technology 1", "Technology 2", "..."],
+  "your_role": "Your role in the project.",
+  "project_type": "Project Type",
+  "project_url": "https://project-url.com",
+  "logo": {
+    "src": "@images/project/logo.png",
+    "alt": "Project Logo"
+  },
+  "photos": [
+    {
+      "url": "@images/project/photo1.png",
+      "alt": "Photo 1"
+    },
+    {
+      "url": "@images/project/photo2.png",
+      "alt": "Photo 2"
+    },
+    ...
+  ]
+}
+```
+
+### Instructions:
+
+- Fill in the respective fields with accurate information about your project.
+- Ensure that image URLs are correctly specified.
+- Customize the JSON structure to fit the specific needs and details of your project.
+
+---
+
+This template provides a standardized format for documenting projects in your portfolio, ensuring consistency and clarity across entries.
---
+++ webpack.config.js
@@ -0,0 +1,131 @@
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const menuItems = require('./data/menu.json');
+
+const config = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss', '.sass', '.css'],
+        alias: {
+            '@assets': path.resolve(__dirname, './assets'),
+            '@images': path.resolve(__dirname, './assets/images'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+            '@script': path.resolve(__dirname, './src/scripts'),
+        },
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/dist'),
+        },
+        open: true,
+        host: '0.0.0.0',
+        port: 8080,
+        watchFiles: {
+            paths: ['src/**/*.*'],
+            options: {
+                usePolling: true,
+            },
+        },
+    },
+    plugins: [
+        new PugPlugin({
+            pretty: true,
+            entry: {
+                'index': {
+                    import: './src/views/index.pug',
+                    data: require('./data/pages/index.json')
+                },
+                'portfolio/05ru': {
+                    import: './src/layouts/portfolio.pug',
+                    data: require('./data/pages/portfolio/05ru.json')
+                },
+                'portfolio/strikepro': {
+                    import: './src/layouts/portfolio.pug',
+                    data: require('./data/pages/portfolio/strikepro.json')
+                },
+                'portfolio/rc_revolution': {
+                    import: './src/layouts/portfolio.pug',
+                    data: require('./data/pages/portfolio/rc_revolution.json')
+                },
+            },
+            data: {
+                menuItems,
+            },
+            js: {
+                filename: 'assets/js/[name].[contenthash:8].js'
+            },
+            css: {
+                filename: 'assets/css/[name].[contenthash:8].css'
+            },
+            loaderOptions: {
+                sources: [
+                    {
+                        tag: 'a',
+                        attributes: ['href'],
+                    },
+                ]
+            }
+        }),
+        // new MiniCssExtractPlugin({
+        //     filename: 'assets/css/[name].[contenthash:8].css',
+        // })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.(png|jpg|jp?g|ico|svg|webp)/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[name].[hash:8][ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext][query]'
+                }
+            },
+            {
+                test: /\.(css|scss|sass)$/i,
+                use: [
+                    'css-loader',
+                    'sass-loader',
+                ],
+                exclude: ['/node_modules/'],
+            },
+        ],
+    },
+    stats: {
+        assets: false,
+        colors: true,
+        version: false,
+        hash: true,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        errorDetails: true
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
